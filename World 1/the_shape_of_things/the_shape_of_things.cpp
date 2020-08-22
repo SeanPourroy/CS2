4b2be21dfa3013233b075ca745066a9f97fa5de9
SeanPourroy <sean.pourroy@shopify.com>
2020-08-22 22:23:26 +0100

    Refactored and moved into functions

--- World 1/the_shape_of_things/the_shape_of_things.cpp
+++ World 1/the_shape_of_things/the_shape_of_things.cpp
@@ -2,33 +2,48 @@
 
 using namespace std;
 
+void leftSpace(int row) {
+  for (int leftSpace = 0; leftSpace < row - 1; leftSpace++) {
+    // We start at 0 space
+    if (row != 1) {
+      cout << ' ';
+    }
+  }
+}
+
+void leftShape(int row) {
+  for (int leftShape = 0; leftShape < row; leftShape++) {
+      cout << '#';
+  }
+}
+
+void spaceMiddle(int row) {
+  for (int spaceMiddle = 0; spaceMiddle <= 20 - (row*4); spaceMiddle++) { 
+    // Skip row 0
+    if (row != 0) {
+      cout << ' ';
+    }
+  }
+}
+
+void rightShape(int row) {
+  for (int rightShape = 0; rightShape < row; rightShape++) {
+    cout << '#';
+  }
+}
+
 int main()
 {
   // First half
   for (int row = 1; row <= 4; row++) {
     // Our left space (0 to 3)
-    for (int leftSpace = 0; leftSpace < row - 1; leftSpace++)
-    {
-      // We start at 0 space
-      if (row != 1) {
-        cout << ' ';
-      }
-    }
+    leftSpace(row);
     // Left shape (1 to 4)
-    for (int leftShape = 0; leftShape < row; leftShape++) {
-      cout << '#';
-    }
+    leftShape(row);
     // Middle space (17 to 5 with a gap of 4)
-    for (int spaceMiddle = 0; spaceMiddle <= 20 - (row*4); spaceMiddle++) { 
-      // Skip row 0
-      if (row != 0) {
-        cout << ' ';
-      }
-    }
+    spaceMiddle(row);
     // Right shape (1 to 4)
-    for (int rightShape = 0; rightShape < row; rightShape++) {
-      cout << '#';
-    }
+    rightShape(row);
     cout << endl;
   }
   // End of first half
@@ -50,24 +65,13 @@ int main()
   // Start of the second half
   for (int row = 4; row > 0; row--) {
     // Our left space (3 to 0)
-    for (int leftSpace = 0; leftSpace < row - 1; leftSpace++) {
-        cout << ' ';
-    }
-    // Middle shape (4 to 1)
-    for (int leftShape = 0; leftShape < row; leftShape++) {
-      cout << '#';
-    }
+    leftSpace(row);
+    // Left shape (4 to 1)
+    leftShape(row);
     // Middle space (5 to 17 with a gap of 4)
-    for (int spaceMiddle = 0; spaceMiddle <= 20 - (row*4); spaceMiddle++) { 
-      // Skip row 0
-      if (row != 0) {
-        cout << ' ';
-      }
-    }
+    spaceMiddle(row);
     // Right shape (4 to 1)
-    for (int rightShape = 0; rightShape < row; rightShape++) {
-      cout << '#';
-    }
+    rightShape(row);
     cout << endl;
   }
   return 0;
